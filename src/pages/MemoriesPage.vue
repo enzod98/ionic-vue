853be71b308b1b981e997480e3207b500dc41160
Enzo <enzod98@hotmail.com>
2021-07-12 21:35:35 -0400

    avances

--- src/pages/MemoriesPage.vue
+++ src/pages/MemoriesPage.vue
@@ -1,22 +1,57 @@
 <template>
     <base-layout pageTitle="Todos los recuerdos">
         <ion-list>
-            <ion-item router-link="/memories/1">Cerro Arco</ion-item>
-            <ion-item>Encarnación</ion-item>
-            <ion-item>Itaipú</ion-item>
-            <ion-item>Barrero</ion-item>
+            <ion-item 
+            v-for="(memory, index) in memories" 
+            :router-link="`/memories/${ memory.id }`"
+            :key="index"
+            >
+                <ion-img></ion-img>
+                {{ memory.title }}
+            </ion-item>
         </ion-list>
     </base-layout>
 </template>
 
 <script>
-import { IonList, IonItem } from '@ionic/vue'
+import { IonList, IonItem, IonImg } from '@ionic/vue'
 
 export default {
     components:{
         IonList,
-        IonItem
-    }
+        IonItem,
+        IonImg
+    },
+    data() {
+        return {
+            memories: [
+                {
+                    id: 'm1',
+                    title: 'Cerro Arco',
+                    description: 'Primer viaje con Sole',
+                    img: 'https://arc-anglerfish-arc2-prod-abccolor.s3.amazonaws.com/public/4ETPJ7MRWZH4PJHQGATGHUWXVQ.jpg'
+                },
+                {
+                    id: 'm2',
+                    title: 'Encarnación',
+                    description: 'El viaje más lejano en familia',
+                    img: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/05/68/71/3d/costanera-de-la-ciudad.jpg?w=700&h=500&s=1'
+                },
+                {
+                    id: 'm3',
+                    title: 'Itaipú',
+                    description: 'Primer viaje con los panas en tercer grado',
+                    img: 'https://upload.wikimedia.org/wikipedia/commons/b/b8/ItaipuAerea2AAL.jpg'
+                },
+                {
+                    id: 'm4',
+                    title: 'Barrero',
+                    description: 'Locura y descontrol con los panas',
+                    img: 'https://www.adndigital.com.py/wp-content/uploads/2017/08/eusebio-ayala-1.jpg'
+                },
+            ]
+        }
+    },
 }
 
 </script>
